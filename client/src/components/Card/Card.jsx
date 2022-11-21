2313725185d459b1e5c9541cc7702556f4cb2c6d
sorotel <agusgimenez097@gmail.com>
2022-11-20 23:18:47 -0300

    esqueleto terminado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -10,7 +10,7 @@ export default function Card({name, image, temperament, weightMin, weightMax, id
             <h3>{name}</h3>
             <h5>{weightMin} - {weightMax} KG</h5>
             <h5>{temperament}{temperaments}</h5>
-            <h5>{id}</h5>
+            
         </div>
     );
 }
